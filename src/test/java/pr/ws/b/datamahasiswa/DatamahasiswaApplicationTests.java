3b388f8bf7d49e3892f0ce704ca022a1e0c60006
alamnurcahaya <alamnurcahaya@10.69.3.182>
2022-10-31 11:28:14 +0700

    V1.0: Membuat project

---
+++ src/test/java/pr/ws/b/datamahasiswa/DatamahasiswaApplicationTests.java
@@ -0,0 +1,13 @@
+package pr.ws.b.datamahasiswa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamahasiswaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
